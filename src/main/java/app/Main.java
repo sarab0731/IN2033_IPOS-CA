823fc58673922ce5b1ef5a258c584a3b7bbf9796
Talip06 <Talip.Tun@city.ac.uk>
2026-03-12 14:08:05 +0000

    Created the database schema, and initialised connection. Added necessary classes for set up

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,8 @@
 package app;
 
+import database.DatabaseSetup;
+import database.UserTest;
 import ui.MainFrame;
-
 import javax.swing.*;
 
 public class Main {
@@ -10,5 +11,12 @@ public class Main {
             MainFrame frame = new MainFrame();
             frame.setVisible(true);
         });
+
+        DatabaseSetup.initialise();
+
+        UserTest.insertUser("admin", "admin123", "Admin User", "ADMIN");
+        UserTest.insertUser("pharm1", "pass123", "Alice Smith", "PHARMACIST");
+
+        UserTest.printUsers();
     }
 }
---
+++ src/main/java/database/UserTest.java
@@ -0,0 +1,64 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserTest {
+
+    public static void insertUser(String username, String passwordHash, String fullName, String role) {
+
+        String sql = """
+                INSERT INTO users (username, password_hash, full_name, role)
+                VALUES (?, ?, ?, ?)
+                """;
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, username);
+            stmt.setString(2, passwordHash);
+            stmt.setString(3, fullName);
+            stmt.setString(4, role);
+
+            stmt.executeUpdate();
+
+            System.out.println("User inserted: " + username);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printUsers() {
+
+        String sql = "SELECT * FROM users";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            System.out.println("---- USERS TABLE ----");
+
+            while (rs.next()) {
+
+                int id = rs.getInt("user_id");
+                String username = rs.getString("username");
+                String fullName = rs.getString("full_name");
+                String role = rs.getString("role");
+
+                System.out.println(
+                        id + " | " +
+                                username + " | " +
+                                fullName + " | " +
+                                role
+                );
+            }
+
+            System.out.println("---------------------");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
